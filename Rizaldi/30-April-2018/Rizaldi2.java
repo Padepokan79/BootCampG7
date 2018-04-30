df1581d0802b7b52972cda22826f663325892f75
rizaldi_r_nensia <1306122@sttgarut.ac.id>
2018-04-30 16:01:11 +0700

    LOGRIZ-30April2018-2

---
+++ Rizaldi/30-April-2018/Rizaldi2.java
@@ -0,0 +1,57 @@
+/*
+     * Display a listing of the resource.
+     *
+     * Programmer : Rizaldi
+     * Created By : Rizaldi
+     * Created At : 30 April 2018 15:45 AM     
+     * Updated By : 
+     * Updated At : 
+*/
+public class Rizaldi2{
+    public static void main( String[] args){
+
+        
+        String happy1, happy2, happy3, happy4, happy5, angry1, angry2, angry3, angry4, angry5, regret1, regret2, regret3, regret4, regret5;
+        happy1 = "| 1. Berkumpul dengan teman/keluarga                                        |";
+        happy2 = "| 2. Bermain atau mendengarkan musik                                        |";
+        happy3 = "| 3. Gajian                                                                 |";
+        happy4 = "| 4. Mancing di sungai dengan ayah                                          |";
+        happy5 = "| 5. brebagi kesenangan dengan orang yang disayang                          |";        
+        angry1 = "| 1. Dibohongi                                                              |";
+        angry2 = "| 2. Dikhianati                                                             |";
+        angry3 = "| 3. Kena macet di tengah terik matahari                                    |";
+        angry4 = "| 4. Mau bersin eh gak jadi                                                 |";
+        angry5 = "| 5. Sedang belajar materinya susah masuk                                   |";
+        regret1 = "| 1. Menyesal gak kuliah di luar negeri                                     |";
+        regret2 = "| 2. Menyesal gak belajar bener pas kuliah                                  |";
+        regret3 = "| 3. Menyesal gak ikut terjun membantu usaha orang tua pas masa SMP dan SMA |";
+        regret4 = "| 4. Menyesal pernah mancing ikan di kolam tetangga                         |";
+        regret5 = "| 5. Menyesal gak jadi astronot                                             |";
+
+        System.out.println("+===========================================================================+");
+        System.out.println("|                       Yang Membuat Saya Bahagia                           |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println(happy1);
+        System.out.println(happy2);
+        System.out.println(happy3);
+        System.out.println(happy4);
+        System.out.println(happy5);
+        System.out.println("+===========================================================================+");
+        System.out.println("|                       Yang Membuat Saya Marah/Kesal                       |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println(angry1);
+        System.out.println(angry2);
+        System.out.println(angry3);
+        System.out.println(angry4);
+        System.out.println(angry5);
+        System.out.println("+===========================================================================+");
+        System.out.println("|               Yang Membuat Saya Menyesal/belum termaafkan                 |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println(regret1);
+        System.out.println(regret2);
+        System.out.println(regret3);
+        System.out.println(regret4);
+        System.out.println(regret5);
+        System.out.println("+---------------------------------------------------------------------------+");
+    }
+}
