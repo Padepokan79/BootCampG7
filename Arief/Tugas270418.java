c30bb8d9ef7010a96e50d715d9bc218935dc300f
apnugraha <arief.pinasti.29@gmail.com>
2018-04-30 16:08:03 -0700

    LOGAPN-30April2018-2.txt

---
+++ Arief/Tugas270418.java
@@ -0,0 +1,86 @@
+/*
+Created Date	: 30 April 2018
+Author			: APN
+Modified Date	:
+Modified By 	:
+*/
+public class Tugas270418 {
+	public static void main(String[]args){
+		String senang1, senang2, senang3, senang4, senang5, senang6, senang7, senang8;
+		String marah1, marah2, marah3, marah4, marah5, marah6;
+		String menyesal1, menyesal2, menyesal3, menyesal4, menyesal5;
+		String soal, wall, nomor, ttlSenang, ttlMarah, ttlMenyesal;
+
+		soal = "Tuliskan Minimal 5 Hal Yang membuat kamu merasa ";
+		wall = "___________________________________________________________";
+		nomor = "|Nomor | Jawaban                                          |";
+		ttlSenang = "Senang !";
+		ttlMarah = "Marah !";
+		ttlMenyesal = "Menyesal !";
+
+		senang1 = "|1.a   |Berkendara keliling kota                          |";
+		senang2 = "|1.b   |Filtering di kemacetan                            |";
+		senang3 = "|1.c   |Touring keluar kota                               |";
+		senang4 = "|1.d   |Menggambar idola                                  |";
+		senang5 = "|1.e   |Mendengarkan musik                                |";
+		senang6 = "|1.f   |Crafting                                          |";
+		senang7 = "|1.g   |Gaming                                            |";
+		senang8 = "|1.h   |Kemah                                             |";
+		
+		marah1 = "|2.a   |Gejala Misophonia kambuh                          |";
+		marah2 = "|2.b   |Menunggu pasukan otewe                            |";
+		marah3 = "|2.c   |Antrian diserobot orang lain                      |";
+		marah4 = "|2.d   |Janji yang diingkari                              |";
+		marah5 = "|2.e   |Pengendara Lampu sein kiri belok kanan            |";
+		marah6 = "|2.f   |Jaringan Tiba-tiba Dissconnect                    |";
+		
+		menyesal1 = "|3.a   |Begadang, tapi lupa beli makanan                  |";
+		menyesal2 = "|3.b   |Belajar bahasa Jepang kurang serius               |";
+		menyesal3 = "|3.c   |Ikut MLM                                          |";
+		menyesal4 = "|3.d   |Tidak banyak belajar sewaktu sekolah              |";
+		menyesal5 = "|3.e   |Lupa cabut charger laptop, sehingga batre rusak   |";
+
+		System.out.println( wall );
+		System.out.println( "" );
+		System.out.println( soal + ttlSenang );
+		System.out.println( wall );
+		System.out.println( nomor );
+		System.out.println( wall );
+		System.out.println( senang1 );
+		System.out.println( senang2 );
+		System.out.println( senang3 );
+		System.out.println( senang4 );
+		System.out.println( senang5 );
+		System.out.println( senang6 );
+		System.out.println( senang7 );
+		System.out.println( senang8 );
+		System.out.println( wall );
+
+		System.out.println( wall );
+		System.out.println( "" );
+		System.out.println( soal + ttlMarah );
+		System.out.println( wall );
+		System.out.println( nomor );
+		System.out.println( wall );
+		System.out.println( marah1 );
+		System.out.println( marah2 );
+		System.out.println( marah3 );
+		System.out.println( marah4 );
+		System.out.println( marah5 );
+		System.out.println( marah6 );
+		System.out.println( wall );
+
+		System.out.println( wall );
+		System.out.println( "" );
+		System.out.println( soal + ttlMenyesal );
+		System.out.println( wall );
+		System.out.println( nomor );
+		System.out.println( wall );
+		System.out.println( menyesal1 );
+		System.out.println( menyesal2 );
+		System.out.println( menyesal3 );
+		System.out.println( menyesal4 );
+		System.out.println( menyesal5 );
+		System.out.println( wall );
+	}
+}
