a409055cec89bd6e86991b17ce99812617038361
richafitria <richafitria1304@gmail.com>
2018-04-30 16:20:09 +0700

    edit file variabel

--- Richa/30-04-2018/RichaFitria.java
+++ Richa/30-04-2018/RichaFitria.java
@@ -1,46 +1,88 @@
 public class RichaFitria {
 	public static void main (String[] args) {
+
+		String hobi1, hobi2, hobi3, hobi4, hobi5, hobi6, hobi7, bertemu1, bertemu2, lucky1, lucky2, berhasil;
+		String marah1, marah2, marah3, marah4, marah5, marah6, marah7, marah8, marah9, marah10, marah11, marah12;
+		String menyesal1,menyesal2, menyesal3,menyesal4, menyesal5,menyesal6, menyesal7, menyesal8;
+
+		hobi1 = "Bisa tidur dengan nyenyak";
+		hobi2 = "Bisa makan-makan enak dan sehat";
+		hobi3 = "Menonton film dan drama korea terbaru";
+		hobi4 = "Hangout bareng teman/sahabat";
+		hobi5 = "Travelling/jalan-jalan ke mana saja terutama ke tempat-tempat baru\n     dan menantang";
+		hobi6 = "Basket dan dancing bareng teman-teman";
+		hobi7 = "Shopping";
+		bertemu1 = "Bisa bertemu dengan orangtua dalam keadaan sehat";
+		bertemu2 = "Bisa bertemu dengan pacar hehe";
+		lucky1 = "Mendapatkan rejeki dadakan"; 
+		lucky2 = "Libur tanggal merah selain hari Minggu";
+		berhasil = "Ketika berhasil memecahakna suatu persoalan/masalah yang sulit";
+
+		marah1 = "Dicuekin";
+		marah2 = "Janjian dengan seseorang tapi orang tersebut telat lebih dari 15 menit";
+		marah3 = "Dibohongin";
+		marah4 = "Dianggap remeh oleh oranglain";
+		marah5 = "Diposesifin";
+		marah6 = "Dibully dan dikatain dengan kata-kata yang kasar atau tidak sewajarnya";
+		marah7 = "Kalau lagi bekerja bisa diganggu tapi untuk urusan yg penting atau\n    masalah kerjaan";
+		marah8 = "Anggota kelompok ada yg menghilang atau tidak bekerja";
+		marah9 = "Ketika bekerja/ mengerjakan sesuatu paling terganggu dengan suara nyanyian";
+		marah10 = "Kegerahan";
+		marah11 = "Diganggu waktu tidur";
+		marah12 = "Ada yg mau curhat atau ngasih tau tapi nggak jadi";
+
+		menyesal1 = "Gemar membuang waktu dengan kegiatan yang tidak terlalu penting \n    seperti bermain sosial media dan nonton youtube";
+		menyesal2 = "Pernah menyakiti hati orangtua karena suatu hal";
+		menyesal3 = "Menyesal karena lulus kuliah agak telat sehingga mulai bekerja juga\n    agak telat dan jugamenghabiskan uang orangtua untuk biaya kuliah";
+		menyesal4 = "Menyesal tidak belajar programming sejak dulu";
+		menyesal5 = "Menyesal tidak ikut kegiatan BEM terpusat sewaktu menjadi mahasiswa";
+		menyesal6 = "Menyesal tidak menjaga kesehatan mata dari kecil";
+		menyesal7 = "Menyesal mengikuti investasi bodong";
+		menyesal8= "Berbohong kepada orangtua";
+
+
+
 		System.out.println ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println ("1. Tuliskan minimal 5 hal yang membuat saya senang!");
 		System.out.println ("                                                                          ");
-		System.out.println ("1.1 Bisa tidur dengan nyenyak");
-		System.out.println ("1.2 Bisa bertemu dengan orangtua dalam keadaan sehat");
-		System.out.println ("1.3 Bisa makan-makan enak dan sehat");
-		System.out.println ("1.4 Mendapatkan rejeki dadakan");
-		System.out.println ("1.5 Bisa bertemu dengan pacar hehe");
-		System.out.println ("1.6 Menonton film dan drama korea terbaru");
-		System.out.println ("1.7 Ketika berhasil memecahakna suatu persoalan/masalah yang sulit");
-		System.out.println ("1.8 Hangout bareng teman/sahabat");
-		System.out.println ("1.9 Libur tanggal merah selain hari Minggu");
-		System.out.println ("1.10 Travelling/jalan-jalan ke mana saja terutama ke tempat-tempat baru\n     dan menantang");
-		System.out.println ("1.11 Basket dan dancing bareng teman-teman");
-		System.out.println ("1.12 Shopping hehe");
+		System.out.println ("1.1 " + hobi1);
+		System.out.println ("1.2 " + bertemu1);
+		System.out.println ("1.3 " + hobi2);
+		System.out.println ("1.4 " + lucky2);
+		System.out.println ("1.5 " + bertemu2);
+		System.out.println ("1.6 " + hobi3);
+		System.out.println ("1.7 " + berhasil);
+		System.out.println ("1.8 " + hobi4);
+		System.out.println ("1.9 " + lucky1);
+		System.out.println ("1.10 " + hobi5);
+		System.out.println ("1.11 " + hobi6);
+		System.out.println ("1.12 " + hobi7);
 		System.out.println ("---------------------------------------------------------------------------");
 		System.out.println ("2. Tuliskan minimal 5 hal yang membuat saya marah!");
 		System.out.println ("                                                                          ");
-		System.out.println ("2.1 Dicuekin");
-		System.out.println ("2.2 Janjian dengan seseorang tapi orang tersebut telat lebih dari 15 menit");
-		System.out.println ("2.3 Dibohongin");
-		System.out.println ("2.4 Dianggap remeh oleh oranglain");
-		System.out.println ("2.5 Diposesifin");
-		System.out.println ("2.6 Dibully dan dikatain dengan kata-kata yang kasar atau tidak sewajarnya");
-		System.out.println ("2.7 Kalau lagi bekerja bisa diganggu tapi untuk urusan yg penting atau\n    masalah kerjaan");
-		System.out.println ("2.8 Anggota kelompok ada yg menghilang atau tidak bekerja");
-		System.out.println ("2.9 Ketika bekerja/ mengerjakan sesuatu paling terganggu dengan suara nyanyian");
-		System.out.println ("2.10 Kegerahan");
-		System.out.println ("2.11 Diganggu waktu tidur");
-		System.out.println ("2.12 Ada yg mau curhat atau ngasih tau tapi nggak jadi");
+		System.out.println ("2.1 " + marah1);
+		System.out.println ("2.2 " + marah2);
+		System.out.println ("2.3 " + marah3);
+		System.out.println ("2.4 " + marah4);
+		System.out.println ("2.5 " + marah5);
+		System.out.println ("2.6 " + marah6);
+		System.out.println ("2.7 " + marah7);
+		System.out.println ("2.8 " + marah8);
+		System.out.println ("2.9 " + marah9);
+		System.out.println ("2.10 " + marah10);
+		System.out.println ("2.11 " + marah11);
+		System.out.println ("2.12 " + marah12);
 		System.out.println ("---------------------------------------------------------------------------");
 		System.out.println ("3. Tuliskan minimal 5 hal yang membuat saya menyesal atau tidak termaafkan!");
 		System.out.println ("                                                                          ");
-		System.out.println ("3.1 Gemar membuang waktu dengan kegiatan yang tidak terlalu penting \n    seperti bermain sosial media dan nonton youtube");
-		System.out.println ("3.2 Pernah menyakiti hati orangtua karena suatu hal");
-		System.out.println ("3.3 Menyesal karena lulus kuliah agak telat sehingga mulai bekerja juga\n    agak telat dan jugamenghabiskan uang orangtua untuk biaya kuliah");
-		System.out.println ("3.4 Menyesal tidak belajar programming sejak dulu");
-		System.out.println ("3.5 Menyesal tidak ikut kegiatan BEM terpusat sewaktu menjadi mahasiswa");
-		System.out.println ("3.6 Menyesal tidak menjaga kesehatan mata dari kecil");
-		System.out.println ("3.7 Menyesal mengikuti investasi bodong");
-		System.out.println ("3.8 Berbohong kepada orangtua ");
+		System.out.println ("3.1 " + menyesal1);
+		System.out.println ("3.2 " + menyesal2);
+		System.out.println ("3.3 " + menyesal3);
+		System.out.println ("3.4 " + menyesal4);
+		System.out.println ("3.5 " + menyesal5);
+		System.out.println ("3.6 " + menyesal6);
+		System.out.println ("3.7 " + menyesal7);
+		System.out.println ("3.8 " + menyesal8);
 		System.out.println ("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	}
 }
