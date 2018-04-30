dd99f74823ab1e7d6eaf670ced3d1aaf1ed133dd
alifhar <alifharj@gmail.com>
2018-04-30 16:00:21 +0700

    LOGAJ-30April2018-2

---
+++ Alifhar/30April2018/Alifhar.java
@@ -0,0 +1,45 @@
+public class Alifhar {
+	public static void main(String[] args) {
+		String happy="1. Saling membantu dalam kesulitan";
+		String happy2="2. Memiliki banyak teman";
+		String happy3="3. Saling berbagi";
+		String happy4="4. Mengunjungi tempat baru";
+		String happy5="5. Berhasil menyelesaikan persoalan yang sulit";
+		String sad="1. Menyalahkan orang lain ketika terjadi masalah";
+		String sad2="2. Tidak peduli dengan sesama";
+		String sad3="3. Mementingkan diri sendiri";
+		String sad4="4. Ketidakjujuran";
+		String sad5="5. Melihat ketidakalian";
+		String regret="1. Tidak pernah mendaki gunung";
+		String regret2="2. Tidak masuk BEM ketika kuliah";
+		String regret3="3. Tidak mencari beasiswa ketika kuliah";
+		String regret4="4. Kurang bersosialisasi ketika kecil";
+		String regret5="5. Belum menepati janji ke sahabat";
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|           Hal yang membuat senang                |");
+		System.out.println("| "+happy+"               |");
+		System.out.println("| "+happy2+"                         |");
+		System.out.println("| "+happy3+"                                |");
+		System.out.println("| "+happy4+"                       |");
+		System.out.println("| "+happy5+"   |");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|            Hal yang membuat marah                |");
+		System.out.println("| "+sad+" |");
+		System.out.println("| "+sad2+"                    |");
+		System.out.println("| "+sad3+"                     |");
+		System.out.println("| "+sad4+"                                |");
+		System.out.println("| "+sad5+"                          |");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|         Hal yang membuat saya menyesal           |");
+		System.out.println("| "+regret+"                   |");
+		System.out.println("| "+regret2+"                 |");
+		System.out.println("| "+regret3+"          |");
+		System.out.println("| "+regret4+"            |");
+		System.out.println("| "+regret5+"               |");
+		System.out.println("|--------------------------------------------------|");
+		System.out.println("|--------------------------------------------------|");
+	}
+}
