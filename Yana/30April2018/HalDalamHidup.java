1487df813b9ff8bf510a9235f82543150caf1bd7
Yana <yana.has.mail@gmail>
2018-04-30 15:00:48 +0700

    LOGYNY-30April2018

---
+++ Yana/30April2018/HalDalamHidup.java
@@ -0,0 +1,63 @@
+public class HalDalamHidup{
+	public static void main(String[] args){
+
+		String	bahagia_1, bahagia_2, bahagia_3, bahagia_4, bahagia_5,
+				kesal_1, kesal_2, kesal_3, kesal_4, kesal_5, kesal_6,
+				menyesal_1, menyesal_2, menyesal_3, menyesal_4, menyesal_5, menyesal_6;
+
+		bahagia_1	= "| 1. Dapat dipercaya                                  |";
+		bahagia_2 	= "| 2. Jalan-jalan                                      |";
+		bahagia_3 	= "| 3. Berbicara Bahasa Korea                           |";
+		bahagia_4 	= "| 4. Makan mie ramen                                  |";
+		bahagia_5 	= "| 5. Mendapatkan penghargaan                          |";
+
+		kesal_1 	= "| 1. Mencium makanan berbau tajam                     |";
+		kesal_2		= "| 2. Mendengar perkataan jorok                        |";
+		kesal_3		= "| 3. Ada yang ngomongin baik/buruk, kedengar langsung |";
+		kesal_4		= "| 4. Diabaikan                                        |";
+		kesal_5		= "| 5. Dituduh yang tidak benar                         |";
+		kesal_6		= "| 6. DMenunggu lama tanpa kabar                       |";
+
+		menyesal_1	= "| 1. Menjaili seorang teman                           |";
+		menyesal_2 	= "| 2. Diam saat dipalak                                |";
+		menyesal_3	= "| 3. Menjadi MC di acara perpisahan SMK               |";
+		menyesal_4	= "| 4. Kena tilang, bayar di tempat                     |";
+		menyesal_5	= "| 5. Tidak lulus sertifikasi SKKNI                    |";
+		menyesal_6	= "| 6. Tidak menguasai bidang ilmu saat kuliah          |";
+
+
+		System.out.println();
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println("| Hal yang membuat bahagia\t\t\t      |");
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println(bahagia_1);
+		System.out.println(bahagia_2);
+		System.out.println(bahagia_3);
+		System.out.println(bahagia_4);
+		System.out.println(bahagia_5);
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println();
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println("| Hal yang membuat marah/kesal                        |");
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println(kesal_1);
+		System.out.println(kesal_2);
+		System.out.println(kesal_3);
+		System.out.println(kesal_4);
+		System.out.println(kesal_5);
+		System.out.println(kesal_6);
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println();
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println("| Hal yang membuat menyesal                           |");
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println(menyesal_1);
+		System.out.println(menyesal_2);
+		System.out.println(menyesal_3);
+		System.out.println(menyesal_4);
+		System.out.println(menyesal_5);
+		System.out.println(menyesal_6);
+		System.out.println("+-----------------------------------------------------+");
+		System.out.println();
+	}
+}
