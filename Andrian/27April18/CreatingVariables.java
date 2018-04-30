e58b551190d70e22195bc29286b89845129f9c68
rifanandrian <muhamadrifanandrian@gmail.com>
2018-04-30 14:59:20 +0700

    LOGANDR-30April2018

---
+++ Andrian/27April18/CreatingVariables.java
@@ -0,0 +1,32 @@
+/* 	Date 		: 27/4/2018    
+	Time 		: 10:39:50
+	Create by 	: Muhamad Rifan Adrian
+	Edited by 	: 
+				
+	*/
+public class CreatingVariables {
+	public static void main( String[] args ) {
+		int x, y, age, z;
+		double second, e, checking, phi;
+		String firstName, lastName, title, middle;
+
+		x = 10;
+		y = 400;
+		age = 39;
+
+		second = 4.71;
+		e = 2.7182881828485904523536;
+		checking = 1.89;
+
+		firstName = "Graham";
+		lastName = "Mitchell";
+		title = "Mr.";
+
+		System.out.println( "The variable x contains " + x );
+		System.out.println( "The value " + y + " is stored in the variable y." );
+		System.out.println( "The experiment took " + second + " seconds." );
+		System.out.println( "A favorite irrational # is Euler's number: " + e );
+		System.out.println( "Hopefully you have more than $" + checking + "!" );
+		System.out.println( "My name's " +title + " " + firstName + lastName );
+	}
+}
---
+++ Andrian/27April18/Ex2Drills1.java
@@ -0,0 +1,16 @@
+public class Ex2Drills1 {
+	public static void main( String[] args ) {
+	System.out.println( "+----------------------------+" );
+	System.out.println( "|                            |" );
+	System.out.println( "|         SPBU Kircon        |" );
+	System.out.println( "|                            |" );
+	System.out.println( "|    2017-04-27   07:38AM    |" );
+	System.out.println( "|                            |" );
+	System.out.println( "| Litter:           2.564    |" );
+	System.out.println( "| Pertamax/Litter:  Rp 7800  |" );
+	System.out.println( "|                            |" );
+	System.out.println( "| Fuel total:       Rp 20000 |" );
+	System.out.println( "|                            |" );
+	System.out.println( "+----------------------------+" );
+	}
+}
