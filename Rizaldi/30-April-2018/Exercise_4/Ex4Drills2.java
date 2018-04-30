104b69f1743c7fd1f31037372e5bbe27448cfed8
rizaldi_r_nensia <1306122@sttgarut.ac.id>
2018-04-30 14:43:39 +0700

    LOGRIZ-30April2018

---
+++ Rizaldi/30-April-2018/Exercise_4/Ex4Drills2.java
@@ -0,0 +1,15 @@
+/*
+     * Display a listing of the resource.
+     *
+     * Programmer : Rizaldi
+     * Created By : Rizaldi
+     * Created At : 27 April 2017 10:11 AM
+     * Updated By : 
+     * Updated At : 
+*/
+
+public class Ex4Drills2{
+    public static void main( String[] args){
+        System.out.println("Alpha \nBravo \nCharlie \nDelta\n \nEcho Foxtrot \nGolf \nHotel \nIndia\n \nthis is  a test");
+    }    
+}
---
+++ Rizaldi/30-April-2018/Rizaldi.java
@@ -0,0 +1,38 @@
+/*
+     * Display a listing of the resource.
+     *
+     * Programmer : Rizaldi
+     * Created By : Rizaldi
+     * Created At : 30 April 2018 08:10 AM     
+     * Updated By : 
+     * Updated At : 
+*/
+public class Rizaldi{
+    public static void main( String[] args){
+        System.out.println("+===========================================================================+");
+        System.out.println("|                       Yang Membuat Saya Bahagia                           |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("| 1. Berkumpul dengan teman/keluarga                                        |");
+        System.out.println("| 2. Bermain atau mendengarkan musik                                        |");
+        System.out.println("| 3. Gajian                                                                 |");
+        System.out.println("| 4. Mancing di sungai dengan ayah                                          |");
+        System.out.println("| 5. brebagi kesenangan dengan orang yang disayang                          |");
+        System.out.println("+===========================================================================+");
+        System.out.println("|                       Yang Membuat Saya Marah/Kesal                       |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("| 1. Dibohongi                                                              |");
+        System.out.println("| 2. Dikhianati                                                             |");
+        System.out.println("| 3. Kena macet di tengah terik matahari                                    |");
+        System.out.println("| 4. Mau bersin eh gak jadi                                                 |");
+        System.out.println("| 5. Sedang belajar materinya susah masuk                                   |");
+        System.out.println("+===========================================================================+");
+        System.out.println("|               Yang Membuat Saya Menyesal/belum termaafkan                 |");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("| 1. Menyesal gak kuliah di luar negeri                                     |");
+        System.out.println("| 2. Menyesal gak belajar bener pas kuliah                                  |");
+        System.out.println("| 3. Menyesal gak ikut terjun membantu usaha orang tua pas masa SMP dan SMA |");
+        System.out.println("| 4. Menyesal pernah mancing ikan di kolam tetangga                         |");
+        System.out.println("| 5. Menyesal gak jadi astronot                                             |");
+        System.out.println("+---------------------------------------------------------------------------+");
+    }
+}
