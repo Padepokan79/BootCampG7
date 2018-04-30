49df7787d282fd82220ebc44947729515a38235c
alifhar <alifharj@gmail.com>
2018-04-30 15:05:24 +0700

    LOGAJ-30April2018

---
+++ Alifhar/27April2018/Drills/Ex5Drills1.java
@@ -0,0 +1,40 @@
+/*
+	* Created by:	Alifhar Juliansyah
+	* 				27-04-2018	10:45
+	* Updated by:
+	*
+*/
+
+public class Ex5Drills1{
+	public static void main(String[] args){
+		int x, y, age, child;
+		double seconds, e, checking, height;
+		String firstname, lastName, title, child1name, child2name;
+
+		x=10;
+		y=40;
+		age=39;
+		child=2;
+
+		seconds=4.71;
+		e=2.71828182845904523536;
+		checking=1.89;
+		height=172.21;
+
+		firstname="Graham";
+		lastName="Mitchell";
+		title="Mr.";
+		child1name="Deo";
+		child2name="Bright";
+
+		System.out.println("The variable x contains "+x);
+		System.out.println("The value "+y+"is stored in the variable y.");
+		System.out.println("I have "+child+" children");
+		System.out.println("The experiment took "+seconds+" seconds");
+		System.out.println("A favorite irrational # is Euler's number: "+e);
+		System.out.println("Hopefuly you have more than $"+checking+"!");
+		System.out.println("I'm about "+height+" centimeters");
+		System.out.println("My name's "+title+" "+firstname+lastName);
+		System.out.println("The names of my children are "+child1name+" and "+child2name);
+	}
+}
