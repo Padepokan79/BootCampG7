9b5bd34fb25e878bb2259ce460adbf7bbfc9af2d
richafitria <richafitria1304@gmail.com>
2018-04-30 14:59:16 +0700

    LOGRCH-30April2018

---
+++ Richa/27-04-2018/Ex3Drills1.java
@@ -0,0 +1,22 @@
+public class Ex3Drills1 {
+	public static void main(String[] args) {
+		System.out.println ("Alpha");
+		System.out.println ("Baravo");
+
+		System.out.println ("Charlie");
+		System.out.println ("Delta");
+		System.out.println ();
+
+		System.out.println ("Echo");
+		System.out.println ("Foxtrot");
+
+		System.out.println ("Golf");
+		System.out.println ("Hotel");
+		System.out.println ();
+		System.out.println ("India");
+
+		System.out.println ();
+		System.out.println ("This" + " " + "is" + " " + "a"  + " " + "test.");
+		System.out.println ("I am learning" + " " + "Java" + " " + "the Hard Way!");
+	}
+}
---
+++ Richa/27-04-2018/Ex4Drills2.java
@@ -0,0 +1,5 @@
+public class Ex4Drills2 {
+	public static void main(String[] args) {
+		System.out.println ("Alpha\nBaravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis" + " " + "is" + " " + "a"  + " " + "test.");
+	}
+}
