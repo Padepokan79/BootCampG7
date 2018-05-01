5029c45659b60f1fa1b1a0372d75fd77676bb1c6
richafitria <richafitria1304@gmail.com>
2018-05-01 22:50:46 +0700

    LOGRCH-1Mei2018-3

---
+++ Richa/01-05-2018/Richa1.java
@@ -0,0 +1,14 @@
+public class Richa1 {
+	public static void main (String[] args) {
+
+		double waktu, jarak, kecepatan;
+		waktu = 60-10; //menit
+		waktu = waktu/60; //jam
+		jarak = 30; //km
+		kecepatan = jarak/waktu;
+
+		System.out.println ("Seseorang mengendarai mobil ke tempat kerjanya selama 1 jam dengan jarak 30 km.\nJika dia terlambat berangkat 10 menit, kecepatan yang harus digunakan agar sampai ke kantor adalah...");
+		System.out.println ("Kecepatan yang harus ditempuh untuk sampai ke tempat kerjanya adalah " + kecepatan + " km/jam");
+	}
+
+}
