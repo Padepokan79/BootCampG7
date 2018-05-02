1890a0e8086762ad9542f35fe215e42695097094
apnugraha <arief.pinasti.29@gmail.com>
2018-05-02 09:56:08 -0700

    LOGAPN-02Mei2018-2

--- Arief/01-05-2018/OperasiMatematika/Richa/Richa03.java
+++ Arief/01-05-2018/OperasiMatematika/Richa/Richa03.java
@@ -9,19 +9,18 @@ public class Richa03{
 		double uangAni, uangBudi, jlhUang, selisih, ani, budi, banding;
 
 		selisih = 7500;
-		ani = 9;
-		budi = 8;
-		banding = ani - budi;
-		uangAni = ani * selisih / banding;
-		uangBudi = budi * selisih / banding;
-		jlhUang = uangAni + uangBudi;
+		uangAni = (100/100);
+		banding = uangAni - (70.0/100);
+		ani = selisih / banding;
+		budi = 0.7 * ani;
+		jlhUang = ani + budi;
 
 		System.out.println("Selisih uang Ani dan Budi adalah 7500. Jika 10% uang Ani \n" +
 							"diberikan kepada Budi, maka uang Budi menjadi 80% uang Ani \n" +
 							"semula. Berapa jumlah uang keduanya?\n");
 
-		System.out.printf("Uang Ani =  %.0f \n", uangAni);
-		System.out.printf("Uang Ani =  %.0f \n", uangBudi);
+		System.out.printf("Uang Ani =  %.0f \n", ani);
+		System.out.printf("Uang Budi =  %.0f \n", budi);
 		System.out.printf("Jumlah uang keduaanya =  %.0f \n", jlhUang);
 
 	}
