9d17f69d788cbc721ad3d2b4b0a42a001bd709dd
khairil89 <khairil.anwar852@gmail.com>
2018-05-03 08:13:48 +0700

    Update Khairil02.java

--- Arief/SoalLatihan/OperasiMatematika/Khairil/Khairil02.java
+++ Arief/SoalLatihan/OperasiMatematika/Khairil/Khairil02.java
@@ -6,15 +6,16 @@ Modified By		:
 */
 public class Khairil02{
 	public static void main( String[] args){
-		double r7, rNon7, kelipatan, nonKelipatan, phi7, phiNon7;
+		double luas1, luas2, phi7, phiNon7;
+		int r7, rNon7;
 
 		r7 = 7;
-		rNon7 = 3;
-		phi7 = 22/7;
+		rNon7 = 10;
+		phi7 = 22.0/7;
 		phiNon7 = 3.14;
 
-		kelipatan = phi7 * (r7 * r7);
-		nonKelipatan = phiNon7 * (rNon7 * rNon7);
+		luas1 = phi7 * (r7 * r7);
+		luas2 = phiNon7 * (rNon7 * rNon7);
 
 		System.out.println("Seorang guru sedang mengajarkan kepada muridnya untuk menghitung luas \n" +
 							"lingkaran. Setiap masing-masing lingkaran memiliki perhitungan yang \n" +
@@ -26,10 +27,8 @@ public class Khairil02{
 		System.out.println("r2 = " + rNon7);
 		System.out.println("phi1 = " + phi7);
 		System.out.println("phi2 = " + phiNon7);
-		System.out.println("phi1 * r1^2 = " + kelipatan);
-		System.out.println("phi2 * r2^2 = " + nonKelipatan);
+		System.out.println("phi1 * r1^2 = " + luas1);
+		System.out.println("phi2 * r2^2 = " + luas2);
 
 	}
 }
-
-//phi nya cukup satu aja tapi nanti berbeda value
