f45b330d56b36e506c0f2c5ea2edf74d5c685ca0
alifhar <alifharj@gmail.com>
2018-05-04 15:23:14 +0700

    LOGAJ-4May2018

--- Alifhar/3May2018/SoalBoolean/BoolKhairil1.java
+++ Alifhar/3May2018/SoalBoolean/BoolKhairil1.java
@@ -11,17 +11,19 @@ public class BoolKhairil1{
 	public static void main(String[] args){
 		Scanner keyboard=new Scanner(System.in);
 		double nilai1, nilai2;
-		String kelamin, domisili;
+		String belum, kelamin, domisili;
 		boolean boleh;
 
 		System.out.print("\nDomisili (masukkan dalam huruf kecil): ");
 		domisili=keyboard.nextLine();
+		System.out.print("Apakah anda pernah mengikuti seleksi olimpiade (pernah/belum): ");
+		belum=keyboard.next();
 		System.out.print("Nilai Matematika Semester 1: ");
 		nilai1=keyboard.nextDouble();
 		System.out.print("Nilai Matematika Semester 2: ");
 		nilai2=keyboard.nextDouble();
 		
-		boleh=( (domisili.equals("bandung") || domisili.equals("kota bandung")) && nilai1>=8 && nilai2>=8 );
+		boleh=( belum.equals("belum") && (domisili.equals("bandung") || domisili.equals("kota bandung")) && nilai1>=8 && nilai2>=8 );
 		System.out.println("Dapat mengikuti seleksi olimpiade matematika: "+boleh);
 	}
 }
