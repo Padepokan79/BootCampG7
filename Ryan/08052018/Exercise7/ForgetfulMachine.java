f1a1646b22c2a523e1abc30a368cb291c2c52fde
rahmadn24 <r4hmadn@gmail.com>
2018-05-08 16:25:54 +0700

    LOGRAN-08Mei2018-2

---
+++ Ryan/08052018/Exercise7/ForgetfulMachine.java
@@ -0,0 +1,29 @@
+/*
+     * Forgetfulmachine.
+     *
+     * Programmer : Ryan Ahmad N
+     * Created By : Ryan Ahmad N
+     * Created At : 08 May 2018 15:44  
+     * Updated By : 
+     * Updated At : 
+*/
+
+import java.util.Scanner;
+
+public class ForgetfulMachine{
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("What city is the capital of France?");
+		keyboard.next();
+
+		System.out.println("What is 6 multiplied by 7?");
+		keyboard.nextInt();
+
+		System.out.println("Enter a number between 0.0 and 1.0.");
+		keyboard.nextDouble();
+
+		System.out.println("Is there anything else you would like to say?");
+		keyboard.next();
+	}
+}
