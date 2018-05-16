2d70b3f4457666069b2c72c7900073a8233e7c53
MCM\MCM-PC <malikchaudhary122@gmail.com>
2018-05-16 13:25:09 +0700

    LOGMLK-16Mei2018-3

--- Malik/2018-05-15/VikriIf1.java
+++ Malik/2018-05-15/VikriIf1.java
@@ -1,19 +1,25 @@
 /*
-created by : Malik Chaudhary
-time : 15-05-2018 2:28PM
+created by 	: Malik Chaudhary
+time 		: 15-05-2018 2:28PM
+update		: 15-05-2018 8:42PM
+
 Soal :
 seorang nene akan memberikan hadiah mobil apabila cucunya yang laki-laki mempunyai istri 1 dan anak lebih dari 2, 
 atau istri lebih dari 2 & anak 1
 
 */
+
 import java.util.Scanner;
 public class VikriIf1{
 	public static void main(String[] args){
 		Scanner nene = new Scanner(System.in);
 
 		String kelamin;
-		int istri, anak;
+		int istri, anak, satu, dua;
 		boolean cowo;
+
+		satu	= 1;
+		dua		= 2;
 
 		System.out.print("jenis kelamin (pria/wanita) : ");
 		kelamin = nene.next();
@@ -25,7 +31,7 @@ public class VikriIf1{
 
 		cowo = kelamin.equals("pria");
 
-		if ( (cowo && istri == 1 && anak > 2) || ( cowo && istri > 2 && anak == 1 ) ) {
+		if ( (cowo && istri == satu && anak > dua) || ( cowo && istri > dua && anak == satu ) ) {
 			System.out.print("Selamat kamu dapat mobil");
 		}else{
 			System.out.print("Maaf kamu kurang beruntung");
