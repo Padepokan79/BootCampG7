a7b747694fd54aaea322ac700faa9bde6db3825c
khairil89 <khairil.anwar852@gmail.com>
2018-06-24 21:10:06 +0700

    Array List Latihan Lanjut

---
+++ Tugas/TugasLatihan/Khairil/ArrayListLatihanLanjut/src/KartuKredit/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Khairil
+ * @Create 22 Juni 2018 - 11:02
+ *
+ * var jenisKartu inherit dari abstract class
+ * var namaKartu definisi untuk class turunan abstract
+ */
+package KartuKredit;
