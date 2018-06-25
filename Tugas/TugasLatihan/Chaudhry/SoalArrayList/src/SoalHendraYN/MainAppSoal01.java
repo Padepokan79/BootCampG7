3a179bf7801a9e27d80005b0b26a7623fe86c182
chaudhary919 <chaudharynurdhiat@gmail.com>
2018-06-25 16:33:49 +0700

    LOGNCM-25Juni2018

--- Tugas/TugasLatihan/Chaudhry/SoalArrayList/src/SoalHendraYN/MainAppSoal01.java
+++ Tugas/TugasLatihan/Chaudhry/SoalArrayList/src/SoalHendraYN/MainAppSoal01.java
@@ -1,5 +1,6 @@
 /*
  * DATE CREATED    : 22 JUNI 2018
+ * DATE UPDATE     : 24 Juni 2018
  * AUTHOR          : NURDHIAT CHAUDHARY MALIK
  * */
 
@@ -10,25 +11,33 @@ import java.util.ArrayList;
 public class MainAppSoal01 {
 
 	public static void main(String[] args) {
-		int jarakKota = 350, jarakAwal = 0;
-		int kecepatanBedu = 60, kecepatanBudi = 80;
+		int jarakKota = 350, jarakAwal = 0, waktuBertemu = 0, pukulBerangkat = 6, waktuBertemuMenit = 0, waktuBerpapasan = 0;
+		int jarakBedu = 0, jarakBudi = 0, range = 0, kmBedu = 15, kmBudi = 20, menit = 15;
 
 		ArrayList<Integer> arrBedu = new ArrayList<Integer>();
 		ArrayList<Integer> arrBudi = new ArrayList<Integer>();
 		
-		for(int index = jarakAwal; index <= jarakKota; index += 5) {
-			arrBedu.add(index);
+		for(int index = jarakAwal; index <= jarakKota; index += menit) {
+			arrBedu.add(jarakBedu);
+			arrBudi.add(jarakBudi);
+			jarakBedu += kmBedu;
+			jarakBudi += kmBudi;
 		}
-		for(int index = jarakKota; index >= jarakAwal ; index -= 5) {
-			arrBudi.add(index);
+		for(int index = 0; index < arrBedu.size(); index++) {
+			range = arrBedu.get(index) + arrBudi.get(index);
+			if (range == jarakKota) {
+				jarakBedu = arrBedu.get(index);
+				jarakBudi = arrBudi.get(index);
+				waktuBerpapasan = arrBedu.indexOf(arrBedu.get(index))*menit;
+			}	
 		}
+		System.out.println("Jarak Bedu -> "+jarakBedu+"Km - Jarak Budi -> "+jarakBudi+"Km");
+		if (waktuBerpapasan >= 60) {
+			waktuBertemu = waktuBerpapasan/60;
+			waktuBertemuMenit = waktuBerpapasan - (waktuBertemu*60);
+		}
+		System.out.println("Pada pukul : 0"+(waktuBertemu+pukulBerangkat)+"."+waktuBertemuMenit+" pagi");
 		
-		for(int index = 0; index <= arrBedu.size(); index++) {
-			if (arrBedu.get(index) == arrBudi.get(index)) {
-				System.out.println("Berpapasan pada jarak "+arrBedu.get(index));
-			}
-		}
+		
 	}
-	
-
 }
---
+++ Tugas/TugasLatihan/Chaudhry/SoalArrayList/src/SoalHendraYN/Soal02CreditCard.java
@@ -0,0 +1,16 @@
+/*
+ * DATE CREATED    : 24 JUNI 2018
+ * AUTHOR          : NURDHIAT CHAUDHARY MALIK
+ * */
+
+package SoalHendraYN;
+
+public abstract class Soal02CreditCard {
+	String namaCredit;
+	int qtyCreditCard;
+	
+	public Soal02CreditCard(String namaCredit, int qtyCreditCard) {
+		this.namaCredit = namaCredit;
+		this.qtyCreditCard = qtyCreditCard;
+	}
+}
